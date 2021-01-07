dd1f896bb518a392f4665cd687bd60b7a83fb4e0
psivakoti <prasanth.sivakoti@cognizant.com>
2021-01-07 15:26:40 -0800

    Initial commit with two test cases

---
+++ src/main/java/com/example/customer/service/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.example.customer.service.controller;
+
+import com.example.customer.service.Customer;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    ObjectMapper mapper;
+    ArrayList<Customer> customerList;
+    String customersJsonPath = "src/test/data/customers.json"; // 4 customers
+    String customerJsonPath = "src/test/data/existingCustomer.json";
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() throws IOException {
+        mapper = new ObjectMapper();
+        File customersFile = new File(customersJsonPath);
+        customerList = mapper.readValue(customersFile, new TypeReference<ArrayList<Customer>>() {});
+        return customerList;
+    }
+
+    @GetMapping("/customer/{id}")
+    public Customer getCustomerById(@PathVariable String id) throws IOException {
+        mapper = new ObjectMapper();
+        File customersFile = new File(customersJsonPath);
+        customerList = mapper.readValue(customersFile, new TypeReference<ArrayList<Customer>>() {});
+        for (Customer customer : customerList ) {
+            if(customer.getId().equals(id)){
+                return customer;
+            }
+        }
+     return null;
+    }
+}
---
+++ src/test/java/com/example/customer/service/CustomerServiceApplicationTests.java
@@ -0,0 +1,85 @@
+package com.example.customer.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class CustomerServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void contextLoads() {
+	}
+
+	ObjectMapper mapper;
+	ArrayList<Customer> customerList;
+
+	String customersJsonPath = "src/test/data/customers.json"; // 4 customers
+	String customerJsonPath = "src/test/data/existingCustomer.json"; // 1 customer
+	String newCustomerJsonPath = "src/test/data/newCustomer.json"; // 1 customer
+
+
+	@BeforeEach
+	void setUp() throws IOException {
+		initializeCustomersData();
+	}
+
+	// TEST UTILITIES ----------------------------------------------------
+
+	private void initializeCustomersData() throws IOException {
+		mapper = new ObjectMapper();
+		File customersFile = new File(customersJsonPath);
+		customerList = mapper.readValue(customersFile, new TypeReference<ArrayList<Customer>>() {});
+	}
+
+	private String getCustomerJsonString() throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		File customerFile = new File(customerJsonPath);
+		Customer customer = mapper.readValue(customerFile, Customer.class);
+		return mapper.writeValueAsString(customer);
+	}
+
+	private String createCustomerJsonString() throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		File customerFile = new File(newCustomerJsonPath);
+		Customer customer = mapper.readValue(customerFile, Customer.class);
+		return mapper.writeValueAsString(customer);
+	}
+
+	@Test
+	void getCustomers() throws Exception {
+		mockMvc.perform(get("/customers")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(mapper.writeValueAsString(customerList)));
+	}
+
+	@Test
+	void getCustomerById() throws Exception {
+		mockMvc.perform(get("/customer/b8a504e8-7cbd-4a54-9a24-dc1832558162")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").exists());
+	}
+
+}
